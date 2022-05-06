6ab4cca16007effe65a808d91dcf4bc404232c62
Chloe Poon <cpoon870@student.fuhsd.org>
2022-05-06 09:02:15 -0700

    Update Orders.java

--- CapstoneProject/src/Orders.java
+++ CapstoneProject/src/Orders.java
@@ -1,4 +1,9 @@
-
+ /**
+  * The order class will be used to create an order, randomizing between the possible ingredients
+  * 
+  * @author cpoon870
+  * @version 05/03/2022
+  */
 public class Orders {
 
 }
