e4b4eb95a6aec2e33df780814943759b133b1bd6
Iris Chou <ichou318@student.fuhsd.org>
2022-05-06 09:13:22 -0700

    Update Counter, make Interactions an interface

--- CapstoneProject/src/Counter.java
+++ CapstoneProject/src/Counter.java
@@ -1,4 +1,20 @@
+/*
+ * The table that the player can place one dish on
+ * 
+ * @author Iris Chou
+ * @version 5/6/2022
+ */
 
-public class Counter {
+public class Counter extends Interactions{
 
+	private Orders holding;
+	
+	public Counter () {
+		super();
+		holding = null;
+	}
+	
+	public void place(Orders order) {
+		holding = order;
+	}
 }
--- CapstoneProject/src/Interactions.java
+++ CapstoneProject/src/Interactions.java
@@ -1,4 +1,4 @@
 
-public class Interactions {
+public interface Interactions {
 
 }
