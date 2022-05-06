de5867b6ac858f25d7bd2f829779e9cd573f8695
Chloe Poon <cpoon870@student.fuhsd.org>
2022-05-05 23:29:51 -0700

    alpha release

---
+++ CapstoneProject/src/DrawingSurface.java
@@ -0,0 +1,16 @@
+import processing.core.PApplet;
+
+public class DrawingSurface extends PApplet{
+	public DrawingSurface() {
+		
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+}
