2610bbc163a01a5f3a0598c1bb595cdec82fea14
Iris Chou <ichou318@student.fuhsd.org>
2022-05-06 09:20:28 -0700

    Update Disaster.java

--- CapstoneProject/src/Disaster.java
+++ CapstoneProject/src/Disaster.java
@@ -1,4 +1,18 @@
 
-public class Disaster {
+/*
+ * A disaster in the game that the player has to account for and fix in a set amount of time.
+ * Each type of disaster has its own unique fix.
+ * 
+ * @author Iris Chou
+ * @version 5/6/2022
+ */
 
+public class Disaster {
+	private String type;
+	private boolean resolved;
+	
+	public Disaster() {
+		type = "";
+		resolved = false;
+	}
 }
