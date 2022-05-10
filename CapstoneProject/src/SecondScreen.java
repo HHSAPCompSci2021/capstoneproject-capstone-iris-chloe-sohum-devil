9aacb6bc27cf79acef0b8e835b2959ec534327c9
Chloe Poon <cpoon870@student.fuhsd.org>
2022-05-10 10:11:49 -0700

    screens

---
+++ CapstoneProject/src/SecondScreen.java
@@ -0,0 +1,39 @@
+
+
+
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class SecondScreen extends Screen {
+	
+	private DrawingSurface surface;
+	public SecondScreen(DrawingSurface surface) {
+		super(800,600);
+	}
+
+
+	public void spawnNewMario() {
+
+	}
+
+	// The statements in the setup() function 
+	// execute once when the program begins
+	public void setup() {
+
+	}
+
+	// The statements in draw() are executed until the 
+	// program is stopped. Each statement is executed in 
+	// sequence and after the last line is read, the first 
+	// line is executed again.
+	public void draw() {
+		
+	}
+
+	
+}
