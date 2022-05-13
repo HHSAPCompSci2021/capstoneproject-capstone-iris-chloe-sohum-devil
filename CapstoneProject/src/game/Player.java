3033f3ca496bbdf3e17fbfb8d4488c469563fe3f
Iris Chou <ichou318@student.fuhsd.org>
2022-05-12 22:48:14 -0700

    Added comment for Player

--- CapstoneProject/src/game/Player.java
+++ CapstoneProject/src/game/Player.java
@@ -2,4 +2,8 @@ package game;
 
 public class Player {
 
+	/*
+	 * Maybe add a private field of type Orders to act as the thing the person is holding
+	 * And add a public method to clear that field, so the hole can call it after something has been dumped into it
+	 */
 }
