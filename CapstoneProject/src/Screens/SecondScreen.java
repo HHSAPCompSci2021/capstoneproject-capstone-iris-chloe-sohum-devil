68d3e34dc013091c4927ff6804aa3d35cd70a04e
Chloe Poon <cpoon870@student.fuhsd.org>
2022-05-18 11:40:28 -0700

    Update SecondScreen.java

--- CapstoneProject/src/Screens/SecondScreen.java
+++ CapstoneProject/src/Screens/SecondScreen.java
@@ -96,7 +96,6 @@ public class SecondScreen extends Screen {
 	// line is executed again.
 	public void draw() {
 		surface.image(background, 0, 0, 800, 600);
-	//	surface.background(0, 255, 255);
 		grid = new Character[20][20];
 		counter.draw(surface, 50,50);
 		hole.draw(surface, 50, 50);
